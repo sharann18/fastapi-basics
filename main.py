b3f0afb876822ba25d79d4c747938cd46f4ee3e4
Sharann Padmanabhan <sharann18@gmail.com>
2024-02-13 14:56:46 +0530

    implemented basic CRUD operations

--- main.py
+++ main.py
@@ -20,13 +20,21 @@ def find_post(id):
         if p['id'] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 def root():
     return {"message": "welcome to my api"} #fastapi automatically converts this dict to json
 
+
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
+
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
@@ -35,10 +43,42 @@ def create_posts(post: Post):
     my_posts.append(post_dict)
     return {"data": post_dict}
 
+
 @app.get("/posts/{id}") #id is a path parameter
-def get_post(id: int, response: Response):
+def get_post(id: int):
     post = find_post(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} was not found")
+    
     return {"post_details": post}
+
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put('/posts/{id}', status_code=status.HTTP_200_OK)
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    
+    post_dict = post.dict()
+
+    post_dict['id'] = id
+
+    my_posts[index] = post_dict
+
+    return {"data": post_dict}
